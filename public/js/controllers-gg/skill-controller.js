101cebee1cc7d7512053c33e1a7ce818ed7e101b
Victor Herrera <dsvictorh@gmail.com>
2016-09-10 01:37:20 -0600

    Auto select first skill

--- public/js/controllers-gg/skill-controller.js
+++ public/js/controllers-gg/skill-controller.js
@@ -1,6 +1,6 @@
 vh.controller('SkillController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout){
 	$scope.viewerActive = true;
-	$scope.activeSkill = 'Front-End';
+	$scope.activeSkill = 'First';
 	$scope.loadSkill = function(name, e){
 		if($scope.activeSkill != name){
 			$scope.viewerActive = false;
